56e5e77a0698db0926304383cf485fbb2c96dd5c
Vitaly Zaff <vitalyzavgorodnev@gmail.com>
2021-04-13 04:54:21 +0000

    lastIndexOf Assignment

---
+++ last-index-of.js
@@ -0,0 +1,23 @@
+
+function lastIndexOf(arr, val){
+  let result; 
+  let foundMatch;
+  for(let i = arr.length - 1; i >= 0; i--){
+    if (arr[i] === val){
+      result = i;
+      foundMatch = true; 
+      break;
+    }
+  }
+  if(foundMatch){
+    return result;
+  } else {
+    return -1;
+  }
+}
+
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 1), "=?", 3);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 2), "=?", 4);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 3), "=?", -1);
+console.log(lastIndexOf([ 5, 5, 5 ], 5), "=?", 2);
+console.log(lastIndexOf([], 3), "=?", -1);
